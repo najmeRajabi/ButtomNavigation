bc2f50af205d47c70739447ea849b69b962198e7
niha8515 <niha8515@yahoo.com>
2022-06-07 19:47:12 +0430

    add bottom navigation

--- app/src/main/java/com/example/buttomnavigation/ui/MainActivity.kt
+++ app/src/main/java/com/example/buttomnavigation/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.buttomnavigation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.buttomnavigation.R
+import com.example.buttomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment?
+        setupWithNavController(binding.bottomNav, navHostFragment!!.navController)
     }
 }
